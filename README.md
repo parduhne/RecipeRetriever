af67d005d6e2711208393bebc44d3d0afa1d5cf9
Paul Talaga <fuzzpault@gmail.com>
2019-09-21 01:46:28 -0400

    Node.js sample communicates with DB and docker is operational!

--- README.md
+++ README.md
@@ -1,7 +1,21 @@
 # RecipeRetriever
 
-## To build:  (not needed right now)
-docker-compose build basic
+~~## To build:  ~~
+~~docker-compose build basic~~
+
+The docker-compose command below creates 3 docker containers:
+1. postgres database (port 5432)
+2. adminer DB admin system (port 8080)
+3. Node.js server (port 8000, debug 9229)
 
 ## To run:
-docker-compose up
+`docker volume create nodemodules`
+
+`docker-compose up`
+
+## adminer
+Go to http://<docker ip>:8080
+
+Open the docker-compose.yml file to find the user/pass/db.  The host is postgres.
+Once logged in you can load the `PGT-schema.sql` or `PGT-schema-wdata.sql` files to populate the DB.  
+Each will drop the current tables.  TODO: work this into a script to run.  TODO: find where the DB state is!
--- server.js
+++ server.js
@@ -1,5 +1,47 @@
 'use strict';
 
+//Load express module with `require` directive
+// From https://blog.logrocket.com/setting-up-a-restful-api-with-node-js-and-postgresql-d96d6fc892d8/
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 8000
+
+// Set up DB connection
+const knex = require('knex')({
+  client: 'postgresql',
+  connection: {
+    debug: 'true',
+    host: process.env.DB_HOST,
+    port: 5432,
+    user: process.env.DB_USER,
+    password: process.env.DB_PWD,
+    database: process.env.DB_NAME
+  }
+});
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+//Define request response in root URL (/)
+// http://zetcode.com/javascript/knex/
+app.get('/users', function (req, res) {
+  knex.from('Users').select("*")
+	  .then((rows) => res.json( {info: rows }));
+  //console.log(req.query['insert'])
+  //res.json( {info: "bobb"} )
+})
+
+//Launch listening server on port 8081
+app.listen(port, function () {
+  console.log('app listening on port 8000!')
+})
+
+/*
 const knex = require('knex')({
   client: 'postgresql',
   connection: {
@@ -39,3 +81,4 @@ function testDb(){
 }
 
 testDb();
+*/
