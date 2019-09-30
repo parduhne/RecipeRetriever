d672bc5b54c598895e2f9c443f9bd24cd7c78d19
Kristians Kanders <kristians@Kristianss-MacBook-Pro.local>
2019-09-30 14:49:01 -0400

    Remove knex, update schema

--- PGT-schema.sql
+++ PGT-schema.sql
@@ -1,13 +1,5 @@
 -- This should be run with the db selected
 
-DROP TABLE IF EXISTS "Measurements" CASCADE;
-DROP TABLE IF EXISTS "Recipes" CASCADE;
-DROP TABLE IF EXISTS "Ingredients" CASCADE;
-DROP TABLE IF EXISTS "IngredientsRecipes" CASCADE;
-DROP TABLE IF EXISTS "Pantry" CASCADE;
-DROP TABLE IF EXISTS "Units" CASCADE;
-DROP TABLE IF EXISTS "Users" CASCADE;
-
 DROP TABLE IF EXISTS Measurements CASCADE;
 DROP TABLE IF EXISTS Recipes CASCADE;
 DROP TABLE IF EXISTS Ingredients CASCADE;
@@ -17,45 +9,40 @@ DROP TABLE IF EXISTS Units CASCADE;
 DROP TABLE IF EXISTS Users CASCADE;
 
 CREATE TABLE public.Measurements (
-    ID integer NOT NULL,
-    Name character varying NOT NULL,
-    CONSTRAINT Measurements_ID PRIMARY KEY (ID)
+    ID serial NOT NULL PRIMARY KEY,
+    Name character varying NOT NULL
 ) WITH (oids = false);
 
 
 CREATE TABLE public.Recipes (
-    ID integer NOT NULL,
+    ID serial NOT NULL PRIMARY KEY,
     Name character varying NOT NULL,
-    Description text,
-    CONSTRAINT Recipes_ID PRIMARY KEY (ID)
+    Description text
 ) WITH (oids = false);
 
 CREATE TABLE public.Users (
-    ID integer NOT NULL,
+    ID serial NOT NULL PRIMARY KEY,
     Name character varying NOT NULL,
     Email text NOT NULL,
-    Password text NOT NULL,
-    CONSTRAINT Users_ID PRIMARY KEY (ID)
+    Password text NOT NULL
 ) WITH (oids = false);
 
 
 CREATE TABLE public.Ingredients (
-    ID integer NOT NULL,
+    ID serial NOT NULL PRIMARY KEY,
     Name character varying NOT NULL,
     Description text,
     MeasurementID integer,
-    CONSTRAINT Ingredients_ID PRIMARY KEY (ID),
     CONSTRAINT Ingredients_MeasurementID_fkey FOREIGN KEY (MeasurementID) REFERENCES Measurements(ID) NOT DEFERRABLE
 ) WITH (oids = false);
 
 
 
 CREATE TABLE public.IngredientsRecipes (
-    ID integer NOT NULL,
+    ID serial NOT NULL PRIMARY KEY,
     RecipeID integer NOT NULL,
     Size real NOT NULL,
     IngredientID integer NOT NULL,
-    CONSTRAINT IngredientsRecipes_ID PRIMARY KEY (ID),
     CONSTRAINT IngredientsRecipes_IngredientID_fkey FOREIGN KEY (IngredientID) REFERENCES Ingredients(ID) NOT DEFERRABLE,
     CONSTRAINT IngredientsRecipes_RecipeID_fkey FOREIGN KEY (RecipeID) REFERENCES Recipes(ID) NOT DEFERRABLE
 ) WITH (oids = false);
@@ -79,11 +66,10 @@ CREATE TABLE public.Pantry (
 
 
 CREATE TABLE public.Units (
-    ID integer NOT NULL,
+    ID serial NOT NULL PRIMARY KEY,
     Name character varying NOT NULL,
     ConversionFactor real,
     MeasurementID integer,
-    CONSTRAINT Units_ID PRIMARY KEY (ID),
     CONSTRAINT Units_MeasurementID_fkey FOREIGN KEY (MeasurementID) REFERENCES Measurements(ID) NOT DEFERRABLE
 ) WITH (oids = false);
 
