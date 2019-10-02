3bba279d859fb04691a1baf6398387cbc097d5a7
Paul Talaga <talagap@uindy.edu>
2019-10-02 14:54:14 -0400

    API comments

--- server.js
+++ server.js
@@ -50,6 +50,7 @@ app.get('/users', function (request, response) {
   // 192.168.99.100:8000/users?insert_name=Emma&insert_email=emma@gmail.com&insert_password=emma
   // TODO: Does not verify the email isn't associated with a user
   // TODO: Password is in plaintext, change to hashed.
+  // TODO: Split this into a GET and POST
   if(request.query.insert_name && request.query.insert_email && request.query.insert_password){
     const insertUserQuery = "INSERT INTO Users (name, email, password) VALUES ($1, $2, $3)";
     const insertUserData = [request.query.insert_name, request.query.insert_email, request.query.insert_password]
@@ -69,6 +70,21 @@ app.get('/users', function (request, response) {
       .catch(e => console.error(e.stack))
   }
 })
+
+// /measurements - get all with id
+
+// /ingredients -insert- name, description, measurementID
+//                        don't do if similar
+// /ingredients -query- name and or description- return ingredientID
+//                        similar
+//                        begins with
+//
+//app.get('/ingredients')
+
+// /pantry -accept- UserID, IngredientID, Size, Units
+// /pantry -query-  UserID, - returns all items in pantry for that user
+
+// /recipes -accept- Name, Description - list of ingredients (ingredientID, size, units).
 
 // An API to execute a SQL file on the system
 // Ex: http://192.168.99.100:8000/rawapply?file=PGT-schema.sql
