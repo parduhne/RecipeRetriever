3a62d66c460f27f12d89d1987f18130e75b14e5f
Ebube Onaga <onagao@uindy.edu>
2019-10-03 23:07:48 -0400

    insert ingredients api

---
+++ insert.js
@@ -0,0 +1,83 @@
+'use strict';
+
+const Pool = require('pg').Pool
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 8000
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: 5432,
+})
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+
+app.post('/ingredients', function (request, response) {
+	if(request.query.name &&
+	request.query.description &&
+	request.query.MeasurementID){
+	  const insertIngredientsQuery = "IF EXISTS (SELECT * FROM Ingredients WHERE Name = $1 and Description = $2) UPDATE Ingredients SET (Name = $1, Description = $2, MeasurementID = '$3) WHERE Name= $1
+		ELSE INSERT INTO Ingredients (Name, Description, MeasurementID) values ($1, $2, $3)"
+	  const insertIngredientsData = [request.query.name, request.query.description, request.query.MeasurementID]
+	  pool
+	    .query(insertIngredientsQuery, insertIngredientsData)
+	    .then(results => {
+	      response.json({info: results.rows})
+	    })
+	    .catch(e => console.error(e.stack))
+	 }
+	 else{
+		
+	 }
+
+})
+
+
+app.get('/ingredients', function (request, response) {
+	if(request.query.name &&
+	request.query.description){
+	  const getIngredientsQuery = "SELECT id FROM Ingredients where name = $1 and description = $2"
+	  const getIngredientsData = [request.query.name, request.query.description]
+	  pool
+	    .query(getIngredientsQuery, getIngredientsData)
+	    .then(results => {
+	      response.json({info: results.rows})
+	    })
+	    .catch(e => console.error(e.stack))
+	}
+	else{
+		
+	}
+
+})
+
+app.get('/rawapply', function (req, res) {
+  if(req.query.file){
+    var fs = require('fs');
+    var sql = fs.readFileSync(req.query.file).toString();
+    pool
+      .query(sql)
+      .then(queryRes => {
+        console.log(queryRes);
+        res.json ({success: queryRes})
+      })
+  }else{
+    res.json( {info: 0 });
+  }
+})
+
+
+//Launch listening server on port 8000
+app.listen(port, function () {
+  console.log('app listening on port 8000!')
+})
