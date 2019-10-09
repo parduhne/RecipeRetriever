0b9e803474cf4af7be23edb026434988786a19b5
Paul Talaga <talagap@uindy.edu>
2019-10-09 16:47:05 -0400

    recipe get working

--- server/recipes.js
+++ server/recipes.js
@@ -1,15 +1,21 @@
 module.exports.init = function(app,pool){
 
-  /*
+  
   app.post('/recipes', function (request, response) {
     if(request.query.name &&
-    request.query.description){
-      const insertRecipesQuery = "IF EXISTS (SELECT * FROM Recipes WHERE Name = $1 and Description = $2) UPDATE Recipes SET (Name = $1, Description = $2) WHERE Name= $1 ELSE INSERT INTO Recipes (Name, Description) values ($1, $2)"
+    request.query.description &&
+    request.query.ingredients){
+      // TODO: Why do a set if it already there?
+      const insertRecipesQuery = `INSERT INTO Recipes (Name, Description) values ($1, $2)`
       const insertRecipesData = [request.query.name, request.query.description]
 
-      for(var i; i < .lenght;i++){
-        const insertIngredientsRecipesQuery = "INSERT INTO IngredientsRecipes (IngredientID, RecipeID, Size) values (SELECT ID FROM Ingredients WHERE Name =  and Description = , SELECT ID FROM Recipes WHERE Name = $1 and Description = $2, $3)"
-        const insertIngredientsRecipesData = [request.query.IngredientID, request.query.RecipeID, request.query.Size]
+      console.error(request.query.ingredients.length)
+
+      let ingredients = JSON.parse(request.query.ingredients)
+      for(var i = 0; i < ingredients.length; i++){
+        console.error(ingredients[i])
+        //const insertIngredientsRecipesQuery = "INSERT INTO IngredientsRecipes (IngredientID, RecipeID, Size) values (SELECT ID FROM Ingredients WHERE Name =  and Description = , SELECT ID FROM Recipes WHERE Name = $1 and Description = $2, $3)"
+        //const insertIngredientsRecipesData = [request.query.IngredientID, request.query.RecipeID, request.query.Size]
       }
 
       pool
@@ -19,30 +25,24 @@ module.exports.init = function(app,pool){
         })
         .catch(e => console.error(e.stack))
      }
-     else{
-
-     }
+     
 
   })
-  */
+  
 
   app.get('/recipes', function (request, response) {
     if(request.query.id){ // Give back the recipe with that ID including ingredients
-      /*const insertRecipesQuery = "IF EXISTS (SELECT * FROM Recipes WHERE Name = $1 and Description = $2) UPDATE Recipes SET (Name = $1, Description = $2) WHERE Name= $1 ELSE INSERT INTO Recipes (Name, Description) values ($1, $2)"
-      const insertRecipesData = [request.query.name, request.query.description]
-
-      for(var i; i < .lenght;i++){
-        const insertIngredientsRecipesQuery = "INSERT INTO IngredientsRecipes (IngredientID, RecipeID, Size) values (SELECT ID FROM Ingredients WHERE Name =  and Description = , SELECT ID FROM Recipes WHERE Name = $1 and Description = $2, $3)"
-        const insertIngredientsRecipesData = [request.query.IngredientID, request.query.RecipeID, request.query.Size]
-      }
+      const getIngredientsQuery = `SELECT size,ingredientid 
+                                   FROM ingredientsrecipes WHERE recipeid = $1`
+      const getIngredientsData = [request.query.id]
 
       pool
-        .query(insertRecipesQuery, insertRecipesData)
+        .query(getIngredientsQuery, getIngredientsData)
         .then(results => {
           response.json({info: results.rows})
         })
         .catch(e => console.error(e.stack))
-        */
+        
      }else{ // Return all recipes with ids
       const getAllRecipesQuery = "SELECT id,name,description FROM recipes"
       pool
