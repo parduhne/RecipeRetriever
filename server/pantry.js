a0839aa8497409472e99f5e13db74224a02dbb9a
Kristians Kanders <kristians@Kristianss-MacBook-Pro.local>
2019-10-25 11:43:46 -0400

    Updating and cleaning up api's

---
+++ server/pantry.js
@@ -0,0 +1,13 @@
+module.exports.init = function(app,pool){
+    app.post('/pantry/:ingredientID', function (request, response){
+        const ingredientID = request.params.ingredientID
+        let ingredient = 
+        // retrieving ingredient 
+
+        // inserting ingredient
+
+
+    })
+
+    
+}
